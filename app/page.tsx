e5758849f81483c16f1d00f3cc4bd8a28a061231
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2024-11-19 23:27:48 +0500

    updated

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import Countdown from '@/components/countdown'
+
+
+const page = () => {
+  return (
+    <div>
+      <Countdown/>
+    </div>
+  )
+}
+
+export default page
